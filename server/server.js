4d41346b8860c2f59cc61d76e8a506bd25536585
Rony Pablo <ronyalexis0@gmail.com>
2023-03-15 00:02:14 -0700

    adding files for Login/Reg

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config();
+const cookieParser = require('cookie-parser')
+
 const cors = require('cors');
-app.use(cors());
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(cookieParser())
 
 require('./config/mongoose.config');
 
